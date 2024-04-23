e2174f6db680ca8668b4d75ee0a19781497b99a1
MarmikShah634 <mnshah632004@gmail.com>
2024-04-23 18:57:46 +0530

    many things done

--- pdf/views.py
+++ pdf/views.py
@@ -1,6 +1,17 @@
-from django.http import HttpResponse
 from django.shortcuts import render
+import PyPDF2
+from transformers import pipeline
+import re
+
+from pdf.forms import FileForm
 
 # Create your views here.
 def index(request):
-    return HttpResponse('hello')
+    if request.method == 'POST':
+        form = FileForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+            return render(request, 'qapdf.html')
+    else:
+        form = FileForm()
+    return render(request, 'index.html', {'form': form})
--- pdfQA/settings.py
+++ pdfQA/settings.py
@@ -122,3 +122,9 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+MEDIA_ROOT = BASE_DIR / 'media'
+
+STATICFILES_DIRS = [
+    BASE_DIR, "static"
+]
